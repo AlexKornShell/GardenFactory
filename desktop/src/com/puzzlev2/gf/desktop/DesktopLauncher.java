475138053f8ed33395e0275c10f70dd277d470fc
Alex <Ksh_sa@mail.ru>
2019-04-20 11:13:49 +0300

    Initial. LibGDX project.

---
+++ desktop/src/com/puzzlev2/gf/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.puzzlev2.gf.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.puzzlev2.gf.GardenFactory;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new GardenFactory(), config);
+	}
+}
